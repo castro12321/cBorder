50f5a315ee5ec3a1450494005120a19c39477449
Norbert Kawiński <kawa12321@gmail.com>
2013-08-13 09:35:59 +0200

    Removed 2 redundant methods

--- src/castro/cBorder/CMV.java
+++ src/castro/cBorder/CMV.java
@@ -48,14 +48,4 @@ public class CMV
 	{
 		return getMVMgr().loadWorld(world);
 	}
-	
-	
-	public static boolean regenWorldWithRandomSeed(String world)
-	{
-		return regenWorld(world, true, true, null);
-	}
-	public static boolean regenWorld(String world, boolean useNewSeed, boolean randomSeed, String seed)
-	{
-		return getMVMgr().regenWorld(world, useNewSeed, randomSeed, seed);
-	}
 }
